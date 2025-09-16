85b6f7de8956f2388816ac8f716a258617123834
akshaykp123 <akshaykp701@gmail.com>
2025-09-16 18:46:07 +0530

    changed

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -27,7 +27,7 @@ export const fetchAllProducts = createAsyncThunk(
   "/products/fetchAllProducts",
   async () => {
     const result = await axios.get(
-      "${import.meta.env.VITE_API_URL}/api/admin/products/get"
+      `${import.meta.env.VITE_API_URL}/api/admin/products/get`
     );
 
     return result?.data;
